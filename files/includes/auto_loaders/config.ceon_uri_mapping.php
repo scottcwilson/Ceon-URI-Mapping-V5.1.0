11ae3a7f57e8af1e51327618bce2fb21c5bcde8c
Nick Fenwick <neek@nickfenwick.com>
2023-01-16 09:12:33 +0700

    Instantiate link builder before core template stuff.
    Core file init_templates.php may bring in language files that use link
    building, e.g. FOOTER_TEXT_BODY may contain links to EZ pages via
    zen_href_link(). Ceon observer must be registered before these language
    strings are evaluated.

--- files/includes/auto_loaders/config.ceon_uri_mapping.php
+++ files/includes/auto_loaders/config.ceon_uri_mapping.php
@@ -33,7 +33,7 @@ $autoLoadConfig[0][] = array(
     'autoType' => 'class',
     'loadFile' => 'observers/class.ceon_uri_mapping_link_build.php'
      );
-$autoLoadConfig[165][] = array(
+$autoLoadConfig[99][] = array(
     'autoType' => 'classInstantiate',
     'className' => 'CeonUriMappingLinkBuild',
     'objectName' => 'ceon_uri_mapping_link_build'
